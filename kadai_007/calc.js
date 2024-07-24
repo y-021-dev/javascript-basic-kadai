f14cfb0053a4c43ae74c663283479ceb349afb4e
y-021-dev <yunachacha1002@icloud.com>
2024-07-25 01:50:58 +0900

    JavaScriptの基礎を学ぼう_7章課題

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+// 変数numに1～99までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 100) + 1;
+
+if(num % 15 === 0) {
+  console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+else if(num % 5 === 0) {
+  console.log('5の倍数です');
+}
+else {
+  console.log(num);
+}
