7285359d2af694cb7f887ce5cb492472a555fdc3
y-021-dev <yunachacha1002@icloud.com>
2024-07-27 17:44:32 +0900

    関数をMDNで調べて使ってみよう

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date();
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+
+// コンソールで現在日付を表示
+console.log(year+'年'+month+'月'+date+'日');
